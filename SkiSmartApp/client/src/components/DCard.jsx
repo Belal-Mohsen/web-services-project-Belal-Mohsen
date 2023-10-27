b6e00508e8ada5119ce51f80c7a4ffb93e087fe3
Chelsea Yao <szyao416@gmail.com>
2023-10-26 21:06:11 -0400

    Updated DCard to use destructured param

--- SkiSmartApp/client/src/components/DCard.jsx
+++ SkiSmartApp/client/src/components/DCard.jsx
@@ -5,10 +5,7 @@ import {HiOutlineBookmark, HiBookmark} from 'react-icons/hi';
 
 import MapUsingAddress from './MapUsingAddress';
 
-const DCard = (detailsData) => {
-
-    const data = detailsData.detailsData;
-
+const DCard = ({detailsData}) => {
     // bookmark
     const [bookMark, setBookMark] = useState(false);
     const handleBookMark = () => {
@@ -17,23 +14,23 @@ const DCard = (detailsData) => {
     
     return (
         <div className="m-8 mt-0 md:w-[500px] md:h-[570px] rounded-3xl overflow-hidden shadow-lg bg-white  border border-gray-600">
-            <img className="w-full md:h-[250px]" src={data.photo} alt="img"/>
+            <img className="w-full md:h-[250px]" src={detailsData.photo} alt="img"/>
                 <div className="px-6 py-4">
-                    <div className="flex items-center gap-2 font-bold text-xl text-black">{data.name} 
+                    <div className="flex items-center gap-2 font-bold text-xl text-black">{detailsData.name} 
                     <div onClick={handleBookMark} className='cursor-pointer'>
-                    {
-                        bookMark ? <HiBookmark /> : <HiOutlineBookmark/>
-                    }
-                </div>
-                     </div>
+                        {
+                            bookMark ? <HiBookmark /> : <HiOutlineBookmark/>
+                        }
+                    </div>
+                    </div>
                     <ul className='text-black'>
-                        <li className='m-0 p-1 flex gap-1 items-center'> <GiEarthAmerica className='text-black'/> <a href={data?.website} target="_blank" rel="noopener noreferrer">{data?.website}</a> </li>
-                        <li className='m-0 p-1 flex gap-2 items-center'> <FaMapMarkerAlt className='text-black'/> {data.address} <MapUsingAddress address={data.address}/></li>
+                        <li className='m-0 p-1 flex gap-1 items-center'> <GiEarthAmerica className='text-black'/> <a href={detailsData?.website} target="_blank" rel="noopener noreferrer">{detailsData?.website}</a> </li>
+                        <li className='m-0 p-1 flex gap-2 items-center'> <FaMapMarkerAlt className='text-black'/> {detailsData.address} <MapUsingAddress address={detailsData.address}/></li>
                         <li className='m-0 p-1'> <span className='font-bold underline'>weather report:</span> </li>
-                        <li className='m-0 p-1 ml-10'><span className='font-semibold'>Temperature:</span> {data.temp}</li>
-                        <li className='m-0 p-1 ml-10'><span className='font-semibold'>Weather Forecast:</span> {data.wForecast}</li>
-                        <li className='m-0 p-1 ml-10'><span className='font-semibold'>Wind Speed:</span> {data.wind}</li>
-                        <li className='m-0 p-1 ml-10'><span className='font-semibold'>Snow Depth: </span> {data.sDepth}</li>
+                        <li className='m-0 p-1 ml-10'><span className='font-semibold'>Temperature:</span> {detailsData.temp}</li>
+                        <li className='m-0 p-1 ml-10'><span className='font-semibold'>Weather Forecast:</span> {detailsData.wForecast}</li>
+                        <li className='m-0 p-1 ml-10'><span className='font-semibold'>Wind Speed:</span> {detailsData.wind}</li>
+                        <li className='m-0 p-1 ml-10'><span className='font-semibold'>Snow Depth: </span> {detailsData.sDepth}</li>
                     </ul>
                 </div> 
         </div>
