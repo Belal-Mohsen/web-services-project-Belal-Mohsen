628e419f9e2faf458812b250df35c69ec7639122
Chelsea Yao <szyao416@gmail.com>
2023-10-26 23:14:02 -0400

    updated signinForm with bg white&opacity

--- SkiSmartApp/client/src/components/SigninForm.jsx
+++ SkiSmartApp/client/src/components/SigninForm.jsx
@@ -24,16 +24,16 @@ const SigninForm = () => {
   };
 
   return (
-      <form onSubmit={handleSubmit} className="text-left w-full max-w-md text-white">
+      <form onSubmit={handleSubmit} className="bg-white bg-opacity-80 p-12 rounded-lg shadow-md w-1/2 max-w-xl">
         <div className="mb-4">
         <div className = "flex">
-        <h1 className="text-2xl font-semibold mb-4">Welcome Back!</h1>
-        <MdWavingHand size = {32} color = "yellow" style = {{marginLeft: "8px"}}/>
+        <h1 className="text-3xl font-semibold mb-4 text-gray-700">Welcome Back!</h1>
+        <MdWavingHand size = {32} color = "blue-300" style = {{marginLeft: "8px"}}/>
         </div>
-          <h4 className="font-semibold mb-6">
+          <h4 className="mb-6 text-xl text-gray-700">
               Today is a new day. It's your day. You shape it.
            </h4>
-          <label htmlFor="email" className="block mb-2">
+          <label htmlFor="email" className="block mb-2 text-gray-700">
             Email:
           </label>
           <input
@@ -48,7 +48,7 @@ const SigninForm = () => {
           />
         </div>
         <div className="mb-4">
-          <label htmlFor="password" className="block mb-2">
+          <label htmlFor="password" className="block mb-2 text-gray-700">
             Password:
           </label>
           <input
@@ -61,7 +61,7 @@ const SigninForm = () => {
             required
             className="w-full border rounded px-3 py-2 focus:outline-none focus:border-blue-500"
           />
-          <a href="/forgot-password" className="text-right text-white hover:underline">
+          <a href="/forgot-password" className="text-right pt-10 text-gray-700 underline">
             Forgot Password?
           </a>
         </div>
@@ -73,7 +73,7 @@ const SigninForm = () => {
         </button>
         <div className="flex items-center justify-center my-4">
           <hr className="flex-grow" />
-          <span className="mx-4 text-white">or</span>
+          <span className="mx-4 text-gray-700">or</span>
           <hr className="flex-grow" />
         </div>
         <div className="mb-4">
